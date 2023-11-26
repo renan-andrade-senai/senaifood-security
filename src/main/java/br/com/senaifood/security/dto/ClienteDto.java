613320555b20fbb315195b0a97cf3dfbc0c68c60
Renan <renan.andrade@edu.sc.senai.br>
2023-11-25 23:21:39 -0300

    Update ClienteDto.java

--- src/main/java/br/com/senaifood/security/dto/ClienteDto.java
+++ src/main/java/br/com/senaifood/security/dto/ClienteDto.java
@@ -2,4 +2,64 @@ package br.com.senaifood.security.dto;
 
 public class ClienteDto {
 
+	private Integer id;
+
+	private String nomeCliente;
+
+	private String enderecoCliente;
+
+	private String emailCliente;
+
+	private String cpfCliente;
+
+	private String telefoneCliente;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public String getEnderecoCliente() {
+		return enderecoCliente;
+	}
+
+	public void setEnderecoCliente(String enderecoCliente) {
+		this.enderecoCliente = enderecoCliente;
+	}
+
+	public String getEmailCliente() {
+		return emailCliente;
+	}
+
+	public void setEmailCliente(String emailCliente) {
+		this.emailCliente = emailCliente;
+	}
+
+	public String getCpfCliente() {
+		return cpfCliente;
+	}
+
+	public void setCpfCliente(String cpfCliente) {
+		this.cpfCliente = cpfCliente;
+	}
+
+	public String getTelefoneCliente() {
+		return telefoneCliente;
+	}
+
+	public void setTelefoneCliente(String telefoneCliente) {
+		this.telefoneCliente = telefoneCliente;
+	}
+
 }
